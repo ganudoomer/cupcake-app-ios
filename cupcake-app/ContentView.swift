da6dd185f46712e3c911d9a4261d48c223dec286
ganudoomer <ganuiscool@gmail.com>
2021-10-24 18:57:52 +0530

    Initial Commit

---
+++ cupcake-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cupcake-app
+//
+//  Created by Sree on 24/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cupcake-app/cupcake_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcake_appApp.swift
+//  cupcake-app
+//
+//  Created by Sree on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct cupcake_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
